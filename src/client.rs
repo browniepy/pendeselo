352897a8285f5024409acac05a33c41dd7acf766
browniepy <yosari.alt@gmail.com>
2024-06-19 15:52:34 -0600

    send method

--- src/client.rs
+++ src/client.rs
@@ -1,27 +1,44 @@
-use crate::{chat::Chat, HashMap};
+use crate::{
+    req_res::{Request, Response},
+    Chat, HashMap, Message, Model,
+};
 
 pub struct Client {
     pub http: ::reqwest::Client,
     pub api_key: String,
     pub chats: HashMap<i64, Chat>,
-}
-
-impl Default for Client {
-    fn default() -> Self {
-        Self {
-            http: ::reqwest::Client::new(),
-            api_key: ::std::env::var("or_key").unwrap(),
-            chats: HashMap::new(),
-        }
-    }
+    pub model: Model,
 }
 
 impl Client {
-    pub fn new(api_key: impl Into<String>) -> Self {
+    pub fn new(api_key: impl Into<String>, model: Model) -> Self {
         Self {
             http: ::reqwest::Client::new(),
             api_key: api_key.into(),
             chats: HashMap::new(),
+            model,
         }
+    }
+
+    pub async fn send(&self, message: Message) -> Result<Response, ::reqwest::Error> {
+        Ok(self
+            .dispatch(Request::new(message, self.model.clone()))
+            .await
+            .unwrap())
+    }
+
+    async fn dispatch(&self, req: Request) -> Result<Response, ::reqwest::Error> {
+        let base = "https://openrouter.ai/api/v1/chat/completions";
+        let response: Response = self
+            .http
+            .post(base)
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {}", self.api_key))
+            .json(&req)
+            .send()
+            .await?
+            .json()
+            .await?;
+        Ok(response)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,26 @@
 pub use ::serde::{Deserialize, Serialize};
 pub use ::std::collections::HashMap;
 
-#[derive(Serialize, Deserialize)]
+mod client;
+pub use client::Client;
+
+pub mod contents;
+pub use contents::Message;
+
+mod chat;
+pub use chat::Chat;
+
+#[derive(Serialize, Deserialize, Clone)]
 #[serde(rename_all = "snake_case")]
 pub enum Role {
     Assistant,
     User,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum Model {
     #[serde(rename = "google/gemma-7b-it:free")]
     Gemma,
 }
 
-pub mod chat;
-pub mod client;
-pub mod contents;
 pub mod req_res;
